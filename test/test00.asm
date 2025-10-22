599318e03ca9987ff2ee0c107cc1ffd31bcc24d5
jjbudz <jeff@furball.com>
2025-10-22 08:38:22 +0200

    Update test/test00.asm with new content

--- test/test00.asm
+++ test/test00.asm
@@ -1,59 +1,62 @@
         ;;  expected result: $022A = 0x55
-$4000   LDAI #085
-        LDXI #042
-        LDYI #115
-        STAZ $81
-        LDAI #$01
-        STAZ $61
-        LDAI #$7E
-        LDAZ $81
-        STAA $0910
-        LDAI #$7E
-        LDAA $0910
-        STAZX $56
-        LDAI #$7E
-        LDAZX $56
-        STYZ $60
-        STAIY $60
-        LDAI #$7E
-        LDAIY $60
-        STAX $07ff
-        LDAI #$7E
-        LDAX $07ff
-        STAY $07ff
-        LDAI #$7E
-        LDAY $07ff
-        STAZX $36
-        LDAI #$7E
-        LDAZX $36
-        STXZ $50
-        LDXZ $60
-        LDYZ $50
-        STXA $0913
-        LDXI #$22
-        LDXA $0913
-        STYA $0914
-        LDYI #$99
-        LDYA $0914
-        STYZX $2D
-        STXZY $77
-        LDYI #$99
-        LDYZX $2D
-        LDXI #$22
-        LDXZY $77
-        LDYI #$99
-        LDYX $0910
-        LDXI #$22
-        LDXY $0914
-        STAX $0200
-
-        ;;  CHECK test00:
-        LDAA $022A
-        CMPI #$55
-        BEQ test00pass
-        JMP theend
-test00pass LDAI #$FE
-           STAA $0210
-theend BRK
-        
-        
+ $4000   LDAI #085
+         LDXI #042
+         LDYI #115
+         STAZ $81
+         LDAI #$01
+         STAZ $61
+         LDAI #$7E
+         LDAZ $81
+         STAA $0910
+         LDAI #$7E
+         LDAA $0910
+         STAZX $56
+         LDAI #$7E
+         LDAZX $56
+         STYZ $60
+         STAIY $60
+         LDAI #$7E
+         LDAIY $60
+         STAX $07ff
+         LDAI #$7E
+         LDAX $07ff
+         STAY $07ff
+         LDAI #$7E
+         LDAY $07ff
+         STAZX $36
+         LDAI #$7E
+         LDAZX $36
+         STXZ $50
+         LDXZ $60
+         LDYZ $50
+         STXA $0913
+         LDXI #$22
+         LDXA $0913
+         STYA $0914
+         LDYI #$99
+         LDYA $0914
+         STYZX $2D
+         STXZY $77
+         LDYI #$99
+         LDYZX $2D
+         LDXI #$22
+         LDXZY $77
+         LDYI #$99
+-        LDYX $0910
++        LDYX $08A0
+         LDXI #$22
+-        LDXY $0914
++        LDXY $08A1
+         STAX $0200
+ 
+         ;;  CHECK test00:
+         LDAA $022A
+-        CMPI #$55
++        CMPA $0200
+         BEQ test00pass
+         JMP theend
+ test00pass LDAI #$FE
+            STAA $0210
+ theend BRK
+         
+         
